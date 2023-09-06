ab3e8a41845601a66079f28da8aa7817834535ee
Alex Krantz <alex@krantz.dev>
2023-09-06 16:57:53 -0700

    Add default logging configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use tracing::Level;
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Setup logging and error reporting
+pub fn init(default_level: Level) {
+    Registry::default()
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(default_level.into())
+                .from_env_lossy(),
+        )
+        .with(ErrorLayer::default())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_file(true)
+                .with_line_number(true)
+                .with_target(true),
+        )
+        .init();
 }
