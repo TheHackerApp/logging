a3c8114ff4e4956a20d04ff24128e88d25eb8484
Alex Krantz <alex@krantz.dev>
2024-01-15 17:43:12 -0800

    Add OpenTelemetry W3C header propagation support

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,6 @@
 use http::Request;
+#[cfg(feature = "opentelemetry")]
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 use tower_http::{
     classify::{ServerErrorsAsFailures, SharedClassifier},
     trace::{DefaultOnRequest, DefaultOnResponse, MakeSpan, TraceLayer},
@@ -7,26 +9,41 @@ use tracing::{span, Level, Span};
 use uuid::Uuid;
 
 /// Creates a custom tracing span
-#[derive(Clone, Copy, Debug)]
-pub struct MakeSpanWithId;
+#[derive(Clone, Debug, Default)]
+pub struct MakeSpanWithId {
+    #[cfg(feature = "opentelemetry")]
+    propagator: TraceContextPropagator,
+}
 
 impl<B> MakeSpan<B> for MakeSpanWithId {
     fn make_span(&mut self, request: &Request<B>) -> Span {
-        span!(
+        let span = span!(
             Level::INFO,
             "request",
             method = %request.method(),
             uri = %request.uri(),
             version = ?request.version(),
             id = %Uuid::new_v4(),
-        )
+        );
+
+        #[cfg(feature = "opentelemetry")]
+        {
+            use opentelemetry::propagation::TextMapPropagator;
+            use opentelemetry_http::HeaderExtractor;
+            use tracing_opentelemetry::OpenTelemetrySpanExt;
+
+            let context = self.propagator.extract(&HeaderExtractor(request.headers()));
+            span.set_parent(context);
+        }
+
+        span
     }
 }
 
 /// Create a logging middleware layer
 pub fn layer() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>, MakeSpanWithId> {
     TraceLayer::new_for_http()
-        .make_span_with(MakeSpanWithId)
+        .make_span_with(MakeSpanWithId::default())
         .on_request(DefaultOnRequest::new().level(Level::INFO))
         .on_response(DefaultOnResponse::new().level(Level::INFO))
 }
